86d73c035379eb0da507ee3c4521034d4df053b1
Luísa <dionatan@bmsoftware.org>
2017-11-27 18:54:40 -0200

    commit para cadastro(básico)

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+@extends('layouts.baseregister')
 
 @section('content')
 <div class="container">
---
+++ resources/views/layouts/base.blade.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>PulsarLicenses</title>
+    <!-- Styles -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
+
+    <!--Import Google Icon Font-->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+</head>
+<body>
+        <a href="{{ action('HomeController@quitandodavida') }}">
+            Logout
+        </a>
+</body>
+</html>
+
+
+
