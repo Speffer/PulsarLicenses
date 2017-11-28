256f994f73d42073378a639512f3148daa14d9fe
Luísa <dionatan@bmsoftware.org>
2017-11-28 16:57:43 -0200

    Login design (avançado)

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,52 +1,48 @@
-@extends('layouts.app')
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>PulsarLicenses Login</title>
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-@section('css-content')
-    <link rel="stylesheet" href="../public/assets/css/base">
-@endsection
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
 
-@section('content')
-    <div class="container">
-        <h4 class="center">Login</h4>
-        <div class="divider"></div>
+    <!-- Styles -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
 
-        <div class="row">
-            <form method="POST" action="{{ route('login')}}">
-                {{ csrf_field() }}
+    <!--Import Google Icon Font-->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 
-                <div class="input-field col s12 {{ $errors->has('email') ? ' has-error' : '' }}">
-                    <input placeholder="E-mail" id="email" type="email" class="validate" name="email" value="{{ old('email') }}">
-                    @if ($errors->has('email'))
-                        <span class="help-block">
-                            <strong>{{ $errors->first('email') }}</strong>
-                        </span>
-                    @endif
-                </div>
-
-                <div class="input-field col s12 {{ $errors->has('password') ? ' has-error' : '' }}">
-                    <input placeholder="Senha" id="password" type="password" class="validate" name="password">
-                    @if ($errors->has('password'))
-                        <span class="help-block">
-                            <strong>{{ $errors->first('password') }}</strong>
-                        </span>
-                    @endif
-                </div>
-                <div class="col s12">
-                    <button class="btn waves-effect waves-light col" type="submit" name="action">Entrar
-                        <i class="material-icons right">send</i>
-                    </button>
-
-                    <p class="col s6">
-                        <input type="checkbox" id="test5" name="remember" {{ old('remember') ? 'checked' : '' }}/>
-                        <label for="test5">Lembrar-se de mim?</label>
-                    </p>
-
-                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                        Esqueceu a senha?
-                    </a>
-                </div>
-
-            </form>
+    <style>
+        html{
+            background-image:url(../public/assets/images/cup-of-coffee.jpg);
+            background-size: cover;
+        }
+    </style>
+</head>
+<body>
+    <nav class="transparent">
+        <div class="nav-wrapper">
+            <a href="#!" class="brand-logo center"></a>
+            <ul class="left hide-on-med-and-down">
+                <li><a href="{{ route('register') }}">Cadastro</a></li>
+                <li><a href="badges.html">PP link</a></li>
+            </ul>
         </div>
-    </div>
-@endsection
+    </nav>
+
+
+
+
+</body>
+<!--Import jQuery before materialize.js-->
+<script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+<!-- Compiled and minified JavaScript -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
+</html>
 
--- resources/views/testelogin.blade.php
+++ resources/views/testelogin.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>PulsarLicenses Login</title>
+    <title>Teste Login</title>
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
@@ -20,24 +20,38 @@
 
     <style>
         html{
-            background-image:url(../public/assets/images/login-register.jpg);
+            background-image:url(../public/assets/images/cup-of-coffee.jpg);
+            background-size: cover;
         }
     </style>
 </head>
 <body>
 
-    <div class="parallax-container">
-        {{--<div class="parallax"><img src="../public/assets/images/login-register.jpg"></div>--}}
-        <nav class="transparent">
-            <div class="nav-wrapper">
-                <a href="#!" class="brand-logo center"></a>
-                <ul class="left hide-on-med-and-down">
-                    <li><a href="{{ route('register') }}">Cadastro</a></li>
-                    <li><a href="badges.html">PP link</a></li>
-                </ul>
-            </div>
-        </nav>
-        <div class="row right">
+    {{--<div class="carousel carousel-slider">--}}
+        {{--<a class="carousel-item" href="#one!"><img src="{{ url('../public/assets/images/address-book.jpg') }})"></a>--}}
+        {{--<a class="carousel-item" href="#two!"><img src="{{ url('../public/assets/images/coder.jpg') }}"></a>--}}
+        {{--<a class="carousel-item" href="#three!"><img src="{{ url('../public/assets/images/cup-of-coffee.jpg') }}"></a>--}}
+        {{--<a class="carousel-item" href="#five!"><img src="{{ url('../public/assets/images/home-office.jpg') }}"></a>--}}
+        {{--<a class="carousel-item" href="#six!"><img src="{{ url('../public/assets/images/laptop.jpg') }}"></a>--}}
+        {{--<a class="carousel-item" href="#seven!"><img src="{{ url('../public/assets/images/office.jpg') }}"></a>--}}
+        {{--<a class="carousel-item" href="#eight!"><img src="{{ url('../public/assets/images/office2.jpg') }}"></a>--}}
+        {{--<a class="carousel-item" href="#nine!"><img src="{{ url('../public/assets/images/people.jpg') }}"></a>--}}
+
+    {{--</div>--}}
+
+    <nav class="transparent">
+        <div class="nav-wrapper">
+            <a href="#!" class="brand-logo center"></a>
+            <ul class="left hide-on-med-and-down">
+                <li><a href="{{ route('register') }}">Cadastro</a></li>
+                <li><a href="badges.html">PP link</a></li>
+            </ul>
+        </div>
+    </nav>
+
+
+    <div class="row right">
+        <div class="valign-wrapper">
             <div class="col s8 m8 l8 right">
                 <h4 class="center white-text">PulsarLicenses</h4>
                 <div class="divider"></div>
@@ -61,27 +75,36 @@
                             @endif
                         </div>
                         <div class="col s12 m12 l12">
-                            <a class="" href="{{ route('password.request') }}">
+                            <a class="brown-text text-darken-1" href="{{ route('password.request') }}">
                                 Esqueceu a senha?
                             </a>
                         </div>
                         <div class="col s12 m12 l12">
                             <p>
                                 <input type="checkbox" id="test5" name="remember" {{ old('remember') ? 'checked' : '' }}/>
-                                <label for="test5">Lembrar de mim?</label>
+                                <label for="test5" class="cyan-text text-accent-3">Lembrar de mim?</label>
                             </p>
                         </div>
                     </div>
-                    <button class="btn waves-effect waves-light col s12 m12 l12" type="submit" name="action">Entrar
+                    <button class="btn waves-effect cyan darken-3 col s12 m12 l12" type="submit" name="action">Entrar
                         <i class="material-icons right">send</i>
                     </button>
                 </form>
             </div>
         </div>
+
     </div>
+
 </body>
 <!--Import jQuery before materialize.js-->
 <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
 <!-- Compiled and minified JavaScript -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
+<script>
+//    $(document).ready(function(){
+//        $('.carousel').carousel();
+//        $('.carousel').carousel('next');
+//        $('.carousel.carousel-slider').carousel({fullWidth: true});
+//    });
+</script>
 </html>
