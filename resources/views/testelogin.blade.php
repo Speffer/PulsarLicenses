4f0f63c96252af003438376a4282c15987afc173
Luísa <dionatan@bmsoftware.org>
2017-11-28 14:41:48 -0200

    Login design (intermediário)

--- resources/views/testelogin.blade.php
+++ resources/views/testelogin.blade.php
@@ -17,56 +17,73 @@
 
     <!--Import Google Icon Font-->
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+    <style>
+        html{
+            background-image:url(../public/assets/images/login-register.jpg);
+        }
+    </style>
 </head>
-<body style="background-image:url(../public/assets/images/login-register.jpg);">
+<body>
 
-
-        <div class="parallax-container">
-            {{--<div class="parallax"><img src="../public/assets/images/login-register.jpg"></div>--}}
-            <nav>
-                <div class="nav-wrapper">
-                    <a href="#!" class="brand-logo center">Pulsar Licenses</a>
-                    <ul class="left hide-on-med-and-down">
-                        <li><a href="{{ route('register') }}">Cadastro</a></li>
-                        <li><a href="badges.html">PP link</a></li>
-                    </ul>
-                </div>
-            </nav>
-            <div class="white col s2 m2 l2 right">
-                <div class="container">
-                    <div class="row">
-                        <div class="col s12 m12 l12">
-                            <h4 class="center">Login</h4>
-                            <div class="divider"></div>
-                            <div class="row">
-                                <form action="{{ route('login')}}" method="POST" class="col s12 m12 l12">
-                                    {{ csrf_field() }}
-                                    <div class="row">
-                                        <div class="input-field col s12 m12 l12">
-                                            <input type="email" placeholder="Email" id="email" name="email" class="validate">
-                                        </div>
-                                        <div class="input-field col s12 m12 l12">
-                                            <input type="password" placeholder="Senha" id="password" name="password" class="validate">
-                                        </div>
-                                    </div>
-                                    <div class="row">
-                                        <div class="col s12 m12 l6">
-                                            <button class="btn waves-effect waves-light col" type="submit" name="action">Entrar
-                                                <i class="material-icons right">send</i>
-                                            </button>
-                                        </div>
-                                        <div class="col s12 m12 l6">
-                                            <a class="btn btn-link" href="{{ route('password.request') }}">
-                                                Esqueceu a senha?
-                                            </a>
-                                        </div>
-                                    </div>
-
-                                </form>
+    <div class="parallax-container">
+        {{--<div class="parallax"><img src="../public/assets/images/login-register.jpg"></div>--}}
+        <nav class="transparent">
+            <div class="nav-wrapper">
+                <a href="#!" class="brand-logo center"></a>
+                <ul class="left hide-on-med-and-down">
+                    <li><a href="{{ route('register') }}">Cadastro</a></li>
+                    <li><a href="badges.html">PP link</a></li>
+                </ul>
+            </div>
+        </nav>
+        <div class="col right">
+            <div class="row">
+                <div class="col s8 m8 l8 right">
+                    <h4 class="center white-text">PulsarLicenses</h4>
+                    <div class="divider"></div>
+                    <form action="{{ route('login')}}" method="POST" class="col s10 m10 l10">
+                        {{ csrf_field() }}
+                        <div class="row">
+                            <div class="input-field col s12 m12 l12 {{ $errors->has('email') ? 'has-error' : '' }} ">
+                                <input type="email" placeholder="Email" id="email" name="email" class="validate">
+                                @if ($errors->has('email'))
+                                    <span class="help-block">
+                                    <strong>{{ $errors->first('email') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                            <div class="input-field col s12 m12 l12 {{ $errors->has('password') ? 'has-error' : '' }}">
+                                <input type="password" placeholder="Senha" id="password" name="password" class="validate">
+                                @if ($errors->has('password'))
+                                    <span class="help-block">
+                                    <strong>{{ $errors->first('password') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                            <div class="col s12 m12 l12">
+                                <a class="" href="{{ route('password.request') }}">
+                                    Esqueceu a senha?
+                                </a>
+                            </div>
+                            <div class="col s12 m12 l12">
+                                <p>
+                                    <input type="checkbox" id="test5" name="remember" {{ old('remember') ? 'checked' : '' }}/>
+                                    <label for="test5">Lembrar de mim?</label>
+                                </p>
                             </div>
                         </div>
-                    </div>
+                        <div class="row">
+                            <div class="col s12 m12 l12">
+                                <button class="btn waves-effect waves-light col" type="submit" name="action">Entrar
+                                    <i class="material-icons right">send</i>
+                                </button>
+                            </div>
+                        </div>
+                    </form>
                 </div>
+            </div>
+
 
 
 
@@ -75,24 +92,16 @@
                         {{--{{ csrf_field() }}--}}
 
                         {{--<div class="col s12 m12 l12">--}}
-                            {{--<div class="input-field col s6 m6 l6 {{ $errors->has('email') ? ' has-error' : '' }}">--}}
+                            {{--<div class="input-field col s6 m6 l6 >--}}
                                 {{--<input placeholder="E-mail" id="email" type="email" class="validate" name="email" value="{{ old('email') }}">--}}
-                                {{--@if ($errors->has('email'))--}}
-                                    {{--<span class="help-block">--}}
-                                    {{--<strong>{{ $errors->first('email') }}</strong>--}}
-                                    {{--</span>--}}
-                                {{--@endif--}}
+
                             {{--</div>--}}
                         {{--</div>--}}
 
                         {{--<div class="col s12 md12 l12">--}}
-                            {{--<div class="input-field col s6 m6 l6 {{ $errors->has('password') ? ' has-error' : '' }}">--}}
+                            {{--<div class="input-field col s6 m6 l6 ">--}}
                                 {{--<input placeholder="Senha" id="password" type="password" class="validate" name="password">--}}
-                                {{--@if ($errors->has('password'))--}}
-                                    {{--<span class="help-block">--}}
-                                    {{--<strong>{{ $errors->first('password') }}</strong>--}}
-                                    {{--</span>--}}
-                                {{--@endif--}}
+
                             {{--</div>--}}
                         {{--</div>--}}
 
@@ -102,10 +111,7 @@
                                 {{--<i class="material-icons right">send</i>--}}
                             {{--</button>--}}
 
-                            {{--<p class="col s3 m3 l3">--}}
-                                {{--<input type="checkbox" id="test5" name="remember" {{ old('remember') ? 'checked' : '' }}/>--}}
-                                {{--<label for="test5">Lembrar-se de mim?</label>--}}
-                            {{--</p>--}}
+
 
                             {{--<a class="btn btn-link" href="{{ route('password.request') }}">--}}
                                 {{--Esqueceu a senha?--}}
@@ -115,8 +121,8 @@
                     {{--</form>--}}
                 {{--</div>--}}
 
-            </div>
         </div>
+    </div>
 
 
 
